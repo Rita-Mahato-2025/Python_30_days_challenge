0b2f3846941ff80b2f2859a832e128e319d033d3
Rita Mahato | Data Analyst <ritamahato.analytics@gmail.com>
2025-05-28 22:45:35 +0530

    Add files via upload

---
+++ Day_2/Day_2_Topics.py
@@ -0,0 +1,11 @@
+#type conversion
+food = "123456.78"
+rent = "888888.88"
+
+#convert the strings to float
+food = float(food)
+rent = float(rent)
+
+#finding total
+total = food + rent
+print(total)
